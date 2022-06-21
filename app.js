4320884c616b57c72e732c867c2e6b7faa3e5fae
KjROX <karnamandal380@gmail.com>
2022-06-21 12:33:18 +0530

    Javascript done!

---
+++ app.js
@@ -0,0 +1,24 @@
+const ratings = Array.from(document.querySelectorAll(".rating"));
+const submitButton = document.querySelector(".pre-submit button");
+const ratingSpanTag = document.querySelector(".your-rating span");
+const postSubmitPage = document.querySelector(".post-submit");
+const preSubmitPage = document.querySelector(".pre-submit");
+let ratedByCustomer;
+function handleRatingClick() {
+  ratedByCustomer = this.getAttribute("value");
+  ratings.forEach((rating) => {
+    rating.classList.remove("rating-clicked");
+  });
+  this.classList.add("rating-clicked");
+}
+function handleSubmitButtonClick() {
+  ratingSpanTag.textContent = ratedByCustomer;
+  postSubmitPage.classList.remove("not-visible");
+  preSubmitPage.classList.add("not-visible");
+}
+
+ratings.forEach((rating) => {
+  rating.addEventListener("click", handleRatingClick);
+});
+
+submitButton.addEventListener("click", handleSubmitButtonClick);
